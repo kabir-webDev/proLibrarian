a9281895f6ddf9d2aaef00eeb19cfbe292eb0957
kabir.webdev <kabir.webdev@gmail.com>
2022-01-15 01:18:23 +0600

    Layout Initialized

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,11 @@
-import SubHomePage from "../Components/SubPage/SubHomePage";
+import User from "../components/User";
 import Layout from "../Layout";
-import axios from "axios";
 
 export default function Home({ blog, comments, projects }) {
   return (
     <Layout>
-      <SubHomePage blog={blog} comments={comments} projects={projects} />
+      <h1 className="text-red-500">This is Something</h1>
+      <User />
     </Layout>
   );
 }
-
-export const getServerSideProps = async (ctx) => {
-  const base_url = process.env.API_URL;
-  const props = {
-    blog: null,
-    comments: [],
-    projects: [],
-  };
-  try {
-    const { data: blogs } = await axios.get(
-      base_url + "authors?populate=image,blogs.images"
-    );
-    props.blog = blogs.data[0] || null;
-
-    const { data: comments } = await axios.get(
-      base_url + "testimonials?populate=*"
-    );
-    props.comments = comments?.data || [];
-
-    const { data: projects } = await axios.get(
-      base_url + "projects?populate=*"
-    );
-    props.projects = projects?.data || [];
-  } catch (e) {}
-  return {
-    props,
-  };
-};
