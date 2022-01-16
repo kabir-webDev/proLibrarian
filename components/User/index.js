8f8a5695ec484915abb112c34d793adf3714c039
kabir.webdev <kabir.webdev@gmail.com>
2022-01-16 23:40:25 +0600

    Backend Connected and adding user functionality done

--- components/User/index.js
+++
@@ -1,4 +0,0 @@
-const User = () => {
-  return <div className="text-red-600">This is user section</div>;
-};
-export default User;
--- pages/404.js
+++
@@ -1,7 +0,0 @@
-import Layout from "../Layout";
-
-function PageNotFound() {
-  return <Layout>Error Occured!!</Layout>;
-}
-
-export default PageNotFound;
